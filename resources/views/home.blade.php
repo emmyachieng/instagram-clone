80565c4bc99aa03763addf73f8ff65dec5c2d337
emmyachieng <emmyachieng97@gmail.com>
2020-10-09 10:40:13 +0300

    instagram-clone first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+       <div class="col-3 p-4">
+          <img class="IalUJ" style="width:150px;height:150px;" src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=RLYRR3IenIMAX8PnaCJ&oh=5f2a88b9b558b7a2d7eb7bcb51c963b2&oe=5F96310F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2">
+       </div>
+       <div class="col-9 pt-4">
+           <div><h1>Amy</h1></div>
+           <div class="d-flex">
+               <div class="pr-5"><strong>123</strong> posts</div>
+               <div class="pr-5"><strong>12k</strong> followers</div>
+               <div class="pr-5"><strong>257</strong> following</div>
+           </div>
+           <div class="pt-4 font-weight-bold">my website.com</div>
+           <div>self taught developer</div>
+           <div><a href="#" style="font-weight-bold;">http:/youtube.com</a></div>
+       </div>
+    </div>
+
+    <div class="row pt-5">
+       <div class="col-4">
+            <img scr="/static/images/mediaUpsell.jpg/6efc710a1d5a.jpg">
+       </div>
+       <div class="col-4">
+            <img src="/static/images/mediaUpsell.jpg/6efc710a1d5a.jpg">
+       </div>
+       <div class="col-4">
+            <img src="mediaUpsell.jpg/6efc710a1d5a.jpg">
+       </div>
+    </div>
+</div>
+@endsection
